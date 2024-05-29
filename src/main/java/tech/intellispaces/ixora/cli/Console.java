187868fa7de50214014e69ae8c151a7516732417
lavor <lavor.andrey@gmail.com>
2024-05-29 10:40:26 +0600

    Minor changes

--- src/main/java/tech/intellispaces/ixora/cli/Console.java
+++ src/main/java/tech/intellispaces/ixora/cli/Console.java
@@ -8,7 +8,7 @@ import tech.intellispacesframework.core.annotation.Transition;
  * Console is a simple text in/outbound system port.
  */
 @Domain("ac5ad94b-0d0b-4bc1-b0af-a32c65a28793")
-@AutoGeneration(enabled = false)
+@AutoGeneration(target = "ConsoleHandle", enabled = false)
 public interface Console {
 
   @Transition("e09d266d-9568-44fd-8ed2-cf27da326a37")
