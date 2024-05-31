84c18a7ab5a70775f0d792e530db008b68ad2bb8
lavor <lavor.andrey@gmail.com>
2024-05-31 17:50:24 +0600

    Package renaming

--- src/main/java/tech/intellispaces/ixora/cli/Console.java
+++ src/main/java/tech/intellispaces/ixora/cli/Console.java
@@ -1,8 +1,8 @@
 package tech.intellispaces.ixora.cli;
 
-import tech.intellispacesframework.core.annotation.AutoGeneration;
-import tech.intellispacesframework.core.annotation.Domain;
-import tech.intellispacesframework.core.annotation.Transition;
+import tech.intellispaces.framework.core.annotation.AutoGeneration;
+import tech.intellispaces.framework.core.annotation.Domain;
+import tech.intellispaces.framework.core.annotation.Transition;
 
 /**
  * Console is a simple text in/outbound system port.
