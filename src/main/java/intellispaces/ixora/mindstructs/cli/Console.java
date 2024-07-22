b15aec950299a39b6f1a0db3538122b610eee637
lavor <lavor.andrey@gmail.com>
2024-07-22 08:47:34 +0600

    Rename to mindstructs

--- src/main/java/intellispaces/ixora/mindstructs/cli/Console.java
+++ src/main/java/intellispaces/ixora/mindstructs/cli/Console.java
@@ -1,4 +1,4 @@
-package intellispaces.ixora.cli;
+package intellispaces.ixora.mindstructs.cli;
 
 import tech.intellispaces.framework.core.annotation.Domain;
 import tech.intellispaces.framework.core.annotation.Transition;
