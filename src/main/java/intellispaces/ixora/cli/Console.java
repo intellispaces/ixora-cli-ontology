0ba4461ca89ffcaf2c9d9301e2496911ec94f2c5
lavor <lavor.andrey@gmail.com>
2024-08-05 10:31:08 +0600

    Console transitions added

--- src/main/java/intellispaces/ixora/cli/Console.java
+++ src/main/java/intellispaces/ixora/cli/Console.java
@@ -11,9 +11,15 @@ import tech.intellispaces.core.traverse.TraverseTypes;
 @Domain("ac5ad94b-0d0b-4bc1-b0af-a32c65a28793")
 public interface Console extends Point {
 
-  @Transition(value = "e09d266d-9568-44fd-8ed2-cf27da326a37", allowedTraverseTypes = TraverseTypes.Moving)
-  Void print(String message);
+  @Transition(value = "e09d266d-9568-44fd-8ed2-cf27da326a37", name = "printString", allowedTraverseTypes = TraverseTypes.Moving)
+  Void print(String string);
 
-  @Transition(value = "0df1747b-b0dc-4a34-9584-54d06af4ffe2", allowedTraverseTypes = TraverseTypes.Moving)
-  Void println(String message);
+  @Transition(value = "5c0ac95a-a8f9-4a77-a6f7-7897659e80b1", name = "printInteger", allowedTraverseTypes = TraverseTypes.Moving)
+  Void print(int number);
+
+  @Transition(value = "0df1747b-b0dc-4a34-9584-54d06af4ffe2", name = "printlnString", allowedTraverseTypes = TraverseTypes.Moving)
+  Void println(String string);
+
+  @Transition(value = "1e2ffbd9-dd81-4fa5-8658-93052623c1b9", name = "printlnInteger", allowedTraverseTypes = TraverseTypes.Moving)
+  Void println(int number);
 }
